174c8d77b9c81cc5e48de3987621cd8d5cc05c78
Ozols <its27eo1@odin.local>
2018-02-26 21:09:33 +0200

    Corrected guest rights

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,11 +23,15 @@
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('products') }}">Products</a>
-                    </li>
-                </ul>
+                @guest
+               
+                @else
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('products') }}">Products</a>
+                        </li>
+                    </ul>
+                @endguest
 
                 <!-- Right Side Of Navbar -->
                 <ul class="navbar-nav ml-auto">
